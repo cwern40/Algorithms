05a2dec8823fcd51769b2aa7d9e67a35b1d2e179
cwern40 <chris.wernli@hotmail.com>
2020-03-03 19:32:44 -0700

    I finished the recipes assignment:

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,8 +3,18 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  batches = []
+  if len(recipe) > len(ingredients):
+    return 0
+  else:
+    for item, need in recipe.items():
+      for item2, available in ingredients.items():
+        if item == item2:
+          batches.append(available // need)
+    
+  return min(batches)
 
+print(recipe_batches({ 'milk': 2, 'sugar': 40, 'butter': 20 }, { 'milk': 5, 'sugar': 120, 'butter': 500 }))
 
 if __name__ == '__main__':
   # Change the entries of these dictionaries to test 
